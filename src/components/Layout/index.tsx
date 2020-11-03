98714166558ec421c4d8ed135f1d5f2216d1ff3e
noberk <noberkli@gmail.com>
2020-11-03 05:51:29 +0000

    add styled-components types

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const CanvasContainer = styled.div`
+  width: 100%;
+  touch-action: none;
+  overflow: auto;
+  background-color: #ffffff;
+  font-size: 0px;
+  outline: none;
+`
---
+++ src/pages/Canvas.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { CanvasContainer } from '../components/Layout'
+export default () => {
+  return (
+    <>
+      <CanvasContainer>123213</CanvasContainer>
+    </>
+  )
+}
